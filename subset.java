0ca3995af3b3b5b114df1c133e60ed0712fcc49e
Karthikeyanchandrasekaran <karthik.nany@gmail.com>
2016-08-25 21:53:00 +0530

    Create subset.java

---
+++ subset.java
@@ -0,0 +1,49 @@
+package reverse;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class unique {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int count=0;
+		ArrayList n=new ArrayList();
+		ArrayList m=new ArrayList();
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String s1=sc.nextLine();
+		for(String a:s.split(" "))
+		{
+			n.add(Integer.parseInt(a));
+			
+		}
+		for(String b:s1.split(" "))
+		{
+			m.add(Integer.parseInt(b));
+		}
+		for(int i=0;i<n.size();i++)
+		{
+			for(int j=0;j<m.size();j++)
+			{
+				if(n.get(i)==m.get(j))
+				{
+					count++;
+				}
+			}
+		}
+		if(count==n.size())
+		{
+			System.out.println("array 1:"+n);
+			System.out.println("array 2:"+m);
+			System.out.println("a1 is the subset of b");
+		}
+		else
+		{
+			System.out.println("a1 is not a subset of b");
+		}
+	}
+
+}
