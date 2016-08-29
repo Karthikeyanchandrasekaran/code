d4aa413772f1fb493c2b3da3ecb68ea7926bd4f5
Karthikeyanchandrasekaran <karthik.nany@gmail.com>
2016-08-29 11:55:48 +0530

    Create spltting.java

---
+++ spltting.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class spltting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList a=new ArrayList();
+		ArrayList b=new ArrayList();
+		int i=0;
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		for(String s:str.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)"))
+		{	
+			if(i==0)
+			{
+			a.add(s);
+			}
+			else if(i==1)
+			{
+				b.add(Integer.parseInt(s));
+			}
+			else if(i%2!=0)
+			{
+				b.add(Integer.parseInt(s));
+			}
+			else
+			{
+				a.add(s);
+			}
+			i++;
+		}
+		for(int j=0;j<b.size();j++)
+		{
+			for(int y=(Integer) b.get(j);y>0;y--)
+			{
+				System.out.print(a.get(j));
+			}
+		}
+
+	}
+
+}
